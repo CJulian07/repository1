31e3e9e78c8726e0090a515d45317e18c2870fae
Carlos Julián Baez <CJulian07@air-de-carlos.bancomer\.com>
2015-12-04 11:48:29 -0600

    Initial Commit

---
+++ pruebaGithub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pruebaGithub
+//
+//  Created by Carlos Julián Baez on 04/12/15.
+//  Copyright © 2015 Carlos Julián Baez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
